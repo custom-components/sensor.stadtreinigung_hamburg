0a689c166fb1855f6f68f8a99ea0ac7eed3a5156
mrestorff <malte@mrestorff.com>
2025-12-22 18:13:34 +0100

    refactor: replace hardcoded platform list with constant in setup functions

--- custom_components/hass_stadtreinigung_hamburg/__init__.py
+++ custom_components/hass_stadtreinigung_hamburg/__init__.py
@@ -1,8 +1,11 @@
 """The Stadtreinigung Hamburg integration"""
+
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 
 DOMAIN = "stadtreinigung_hamburg"
+PLATFORMS = [Platform.SENSOR]
 
 
 async def async_setup(hass, config):
@@ -12,11 +15,10 @@ async def async_setup(hass, config):
 
 async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
     """Set up Stadtreinigung Hamburg as config entry."""
-    await hass.config_entries.async_forward_entry_setups(config_entry, ["sensor"])
+    await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    await hass.config_entries.async_unload_platforms(config_entry, ["sensor"])
-    return True
+    return await hass.config_entries.async_unload_platforms(config_entry, PLATFORMS)
